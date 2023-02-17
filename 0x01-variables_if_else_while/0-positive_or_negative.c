9abcaf87c4b8434e219afd37c33ab98779c39e77
myusername <myemail>
2023-02-17 03:55:27 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,12 +1,10 @@
 #include <stdlib.h>
-
 #include <time.h>
-
 #include <stdio.h>
 
 /**
  * main - Prints if number is positive, zero or negative
- * Return: Always (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
@@ -19,14 +17,14 @@ int main(void)
 
 	if (n > 0)
 	{
-	printf("%d is positive\n", n);
+		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
-	printf("%d is zero\n", n);
+		printf("%d is zero\n", n);
 	}
 	else
 	{
-	printf("%d is negative\n", n);
+		printf("%d is negative\n", n);
 	}
 	return (0);
