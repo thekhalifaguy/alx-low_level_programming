3f3050095165f158a21ae922f0d7f3075563f17e
myusername <myemail>
2023-02-17 08:58:08 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,23 +2,22 @@
 #include <stdlib.h>
 #include <time.h>
 
-
 /**
- * main - Prints the alphabet in upper case and in lower case
+ * main - Entry point
  * Return: Always 0 (Success)
  */
 
 int main(void)
 
 {
-	
+
 	char low;
 
 	for (low = 'a'; low <= 'z'; low++)
 	putchar (low);
 
 	for (low = 'A'; low <= 'Z'; low++)
-	putchar (low)
+	putchar (low);
 	putchar ('\n');
 
 	return (0);
