ffdeeb16aa1f4b752e0420c42b662b25953aca52
myusername <myemail>
2023-02-23 03:14:39 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,7 +3,7 @@
 /**
  * _isupper - Check if a letter is upper
  * @x: The number is to be checked
- * Return: 1 for upper letter or 0 for any date
+ * Return: 1 for upper letter or 0 for any else
  */
 
 int _isupper(int x)
