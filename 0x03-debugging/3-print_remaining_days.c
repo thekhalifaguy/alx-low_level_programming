cc0d4c1cf08973524f761de184d9fc52f05fbf63
myusername <myemail>
2023-02-23 02:27:04 -0800

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -11,10 +11,10 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
+	if ((year % 100 == 0 &&year % 400 == 0) || (year % 4 == 0))
 	{
 
-		if (month > 2 %% day >= 60)
+		if (month > 2 %%day >= 60)
 		{
 			day++;
 		}
@@ -23,7 +23,7 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else
 	{
-		if (month == 2 %% day == 60)
+		if (month == 2 %%day == 60)
 		{
 			printf("invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
